bd67911fa389f379124105511bb7ea8194b05595
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-06-29 10:37:20 +0700

    adjustment core urls

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegistrationView
 
 urlpatterns = [
-    path('signup/', UserRegistrationView.as_view(), name='user-register'),
+    path('signup', UserRegistrationView.as_view(), name='user-register'),
 ]
