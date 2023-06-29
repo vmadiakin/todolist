1d23cfd85a51b700030ef59636e1ecdecb7e7371
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-06-29 15:08:05 +0700

    reload swagger

--- todolist/urls.py
+++ todolist/urls.py
@@ -24,8 +24,8 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version='v1',
-        description="API documentation for your project",
-        terms_of_service="https://www.example.com/terms/",
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
         license=openapi.License(name="BSD License"),
     ),
