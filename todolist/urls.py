9011104861b97e1250808d86650848b13a39eeec
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-06-29 16:40:07 +0700

    swagger settings

--- todolist/urls.py
+++ todolist/urls.py
@@ -30,13 +30,12 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('core/', include('core.urls')),
 ]
