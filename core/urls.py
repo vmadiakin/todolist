6451e66e35e831f38887d78264f777085ed7c99c
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-06-29 14:51:56 +0700

    getting/updating the current user

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserProfileView
+from .views import UserRegistrationView, UserLoginView, UserRetrieveUpdateView
 
 urlpatterns = [
     path('signup', UserRegistrationView.as_view(), name='user-register'),
     path('login', UserLoginView.as_view(), name='user-login'),
-    path('profile', UserProfileView.as_view(), name='profile'),
+    path('profile', UserRetrieveUpdateView.as_view(), name='profile'),
 ]
