3bbbcc142b10df16dc0f9785b350b069896dbe3d
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-07-17 12:57:49 +0700

    goals categories urls update

--- goals/urls.py
+++ goals/urls.py
@@ -4,7 +4,7 @@ from goals import views
 
 
 urlpatterns = [
-    path("goal_category/create", views.GoalCategoryCreateView.as_view()),
-    path("goal_category/list", views.GoalCategoryListView.as_view()),
-    path("goal_category/<pk>", views.GoalCategoryView.as_view()),
+    path("goal_category/create/", views.GoalCategoryCreateView.as_view()),
+    path("goal_category/list/", views.GoalCategoryListView.as_view()),
+    path("goal_category/<pk>/", views.GoalCategoryView.as_view()),
 ]
