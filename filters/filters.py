4a166cda4b189a53d41df470e4746ccc6c3b1025
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-07-19 15:36:28 +0700

    Goals filters date

--- filters/filters.py
+++ filters/filters.py
@@ -1,28 +1,20 @@
 import django_filters
 from django.db import models
+from django_filters import rest_framework
+
 from goals.models import Goal
 
 
-class GoalDateFilter(django_filters.FilterSet):
-    due_date__gte = django_filters.DateFilter(field_name='due_date', lookup_expr='gte')
-    due_date__lte = django_filters.DateFilter(field_name='due_date', lookup_expr='lte')
-    category = django_filters.CharFilter(lookup_expr='exact')
-    status = django_filters.NumberFilter(lookup_expr='exact')
-    priority = django_filters.NumberFilter(lookup_expr='exact')
-
+class GoalDateFilter(rest_framework.FilterSet):
     class Meta:
         model = Goal
         fields = {
-            "category": ["exact", "in"],
-            "status": ["exact", "in"],
-            "priority": ["exact", "in"],
+            "due_date": ("lte", "gte"),
+            "category": ("exact", "in"),
+            "status": ("exact", "in"),
+            "priority": ("exact", "in"),
         }
 
     filter_overrides = {
         models.DateTimeField: {"filter_class": django_filters.IsoDateTimeFilter},
     }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.filters['due_date__lte'].label = 'due_date__lte'
-        self.filters['due_date__gte'].label = 'due_date__gte'
