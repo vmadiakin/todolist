6e9f665765025f95afb6ffa472fc66f8861f2d1c
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-07-19 14:11:26 +0700

    Goals filters date

--- filters/filters.py
+++ filters/filters.py
@@ -4,17 +4,11 @@ from goals.models import Goal
 
 
 class GoalDateFilter(django_filters.FilterSet):
-    due_date = django_filters.DateFromToRangeFilter(field_name='due_date')
-    due_date_exact = django_filters.DateFilter(field_name='due_date', lookup_expr='exact')
-    category = django_filters.CharFilter(lookup_expr='exact')
-    status = django_filters.NumberFilter(lookup_expr='exact')
-    priority = django_filters.NumberFilter(lookup_expr='exact')
+    due_date = django_filters.DateFromToRangeFilter()
 
     class Meta:
         model = Goal
         fields = {
-            'due_date': ['range'],  # Используем 'range' для фильтрации диапазона дат
-            'due_date_exact': ['exact'],  # Используем 'exact' для фильтрации конкретной даты
             "category": ["exact", "in"],
             "status": ["exact", "in"],
             "priority": ["exact", "in"],
@@ -23,3 +17,12 @@ class GoalDateFilter(django_filters.FilterSet):
     filter_overrides = {
         models.DateTimeField: {"filter_class": django_filters.IsoDateTimeFilter},
     }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters["due_date"].lookup_expr = "range"  # Изменяем lookup_expr на "range"
+
+        # Изменяем порядок полей "С" и "ДО" в форме фильтрации
+        due_date_field = self.filters["due_date"].field
+        due_date_field.widget.widgets[0].attrs["placeholder"] = "ДО"  # Изменяем placeholder для "ДО"
+        due_date_field.widget.widgets[1].attrs["placeholder"] = "С"  # Изменяем placeholder для "С"
