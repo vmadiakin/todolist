d87ad1b405134200e5624af3936e7cb83eb2e3fd
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-07-19 13:31:24 +0700

    Goals filters date

--- filters/filters.py
+++ filters/filters.py
@@ -3,8 +3,17 @@ from django.db import models
 from goals.models import Goal
 
 
+class DateFromToRangeFilter(django_filters.DateFromToRangeFilter):
+    def filter(self, qs, value):
+        if value and value.start:
+            qs = qs.filter(**{'{}__gte'.format(self.field_name): value.start})
+        if value and value.stop:
+            qs = qs.filter(**{'{}__lte'.format(self.field_name): value.stop})
+        return qs
+
+
 class GoalDateFilter(django_filters.FilterSet):
-    due_date = django_filters.DateRangeFilter()
+    due_date = DateFromToRangeFilter()
     category = django_filters.CharFilter(lookup_expr='exact')
     status = django_filters.NumberFilter(lookup_expr='exact')
     priority = django_filters.NumberFilter(lookup_expr='exact')
@@ -12,7 +21,7 @@ class GoalDateFilter(django_filters.FilterSet):
     class Meta:
         model = Goal
         fields = {
-            "due_date": ["date__gte", "date__lte"],
+            "due_date": ["gte", "lte"],
             "category": ["exact", "in"],
             "status": ["exact", "in"],
             "priority": ["exact", "in"],
