96e82d300d50a98aba2cdcc587f0e03440495757
Vitalii Madiakin <madyakinvitalii@gmail.com>
2023-07-19 14:40:59 +0700

    Goals filters date

--- filters/filters.py
+++ filters/filters.py
@@ -1,23 +1,18 @@
 import django_filters
-from django.db import models
 from goals.models import Goal
 
 
 class GoalDateFilter(django_filters.FilterSet):
-    due_date = django_filters.DateFromToFilter()
-    category = django_filters.CharFilter(lookup_expr='exact')
-    status = django_filters.NumberFilter(lookup_expr='exact')
-    priority = django_filters.NumberFilter(lookup_expr='exact')
+    due_date = django_filters.DateFromToRangeFilter(field_name="due_date")
+
+    due_date__gte = django_filters.DateFilter(field_name="due_date", lookup_expr='gte')
+    due_date__lte = django_filters.DateFilter(field_name="due_date", lookup_expr='lte')
 
     class Meta:
         model = Goal
         fields = {
-            "due_date": ["lte", "gte"],
+            "due_date": ["exact"],
             "category": ["exact", "in"],
             "status": ["exact", "in"],
             "priority": ["exact", "in"],
         }
-
-    filter_overrides = {
-        models.DateTimeField: {"filter_class": django_filters.IsoDateTimeFilter},
-    }
